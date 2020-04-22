d4e2a7abc8c494e03f93b5ffef1b1c12b5630a7c
Hyeok Yun <hyoeky98@gmail.com>
2020-04-23 01:47:53 +0900

    Child

--- ReferenceTypes/ReferenceTypes.java
+++ ReferenceTypes/ReferenceTypes.java
@@ -6,4 +6,10 @@ public class ReferenceTypes{
 
 	}
 	
+	public void makeMemberObject(){
+		MemberDTO dto1 = new MemberDTO();
+		MemberDTO dto2 = new MenberDTO("Sangmin");
+		MemberDTO dto2 = new MenberDTO("Sangmin", "010XXXXYYYY");
+		MemberDTO dto2 = new MenberDTO("Sangmin", "010XXXXYYYY", "god@godofjava.com");
+	}
 }
--- inheritance/Child.java
+++ inheritance/Child.java
@@ -5,9 +5,11 @@ public class Child extends Parent {
 		super(null);
 		System.out.println("Child Constructor");
 	}
+	public Child(String name) {
+		super(name);
+		System.out.println("Child(String) Constructor");
+	}
 	public void printAge() {
 		System.out.println("printAge() - 18 month");
 	}
 }
-
-		
