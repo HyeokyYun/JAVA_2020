20359677880b1782d6494cdd7f82cb8df0720732
Hyeok Yun <hyoeky98@gmail.com>
2020-04-23 01:54:57 +0900

    Parent

--- inheritance/Parent.java
+++ inheritance/Parent.java
@@ -4,14 +4,13 @@ public class Parent{
 	//public Parent() {
 	//	System.out.println("Parent Constructor");
 	//}
-	public Parent(String name) {
-		System.out.println("Parent(String) Constructor");
-	}
-	public Parent() {
-		System.out.println("Parent(Interitance) Constructor");
-	}
+	//public Parent(String name) {
+	//	System.out.println("Parent(String) Constructor");
+	//}
+	//public Parent() {
+	//	System.out.println("Parent(Inheritance) Constructor");
+	//}
 	public void printName() {
 		System.out.println("printName() - Parent");
 	}
 }
-
