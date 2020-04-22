7a6636b6de9b8e87baebd0c64fa5120c36e4ff3e
Hyeok Yun <hyoeky98@gmail.com>
2020-04-23 01:44:15 +0900

    Child

---
+++ inheritance/Child.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Child extends Parent {
+	public Child() {
+		super(null);
+		System.out.println("Child Constructor");
+	}
+	public void printAge() {
+		System.out.println("printAge() - 18 month");
+	}
+}
+
+		
