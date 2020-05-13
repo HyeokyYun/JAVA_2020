afe3b58fc919361b021f9ed2523eb5bde7879262
HyeokyYun <hyoeky98@gmail.com>
2020-05-14 07:49:12 +0900

    Exer2

---
+++ Recursion/Exer2.java
@@ -0,0 +1,18 @@
+public class Main{
+		public static void main(String [] args)
+				{
+							methodA(3);
+								}
+			public static void methodA(int n)
+					{
+								if(n < 1)
+												System.out.println('B');
+										else{
+														System.out.println('R');
+																	methodA(n-1);
+																				//System.out.println('R');
+																			}
+
+											}
+
+}
